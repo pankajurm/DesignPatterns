10114a2ac806ce61d380767b5919added98216d6
Pankaj <pankajuramaliya19@gmail.com>
2026-09-12 23:38:42 +0530

    defect fix-3456-loin-error

--- DP/src/ABC/C.java
+++ DP/src/ABC/C.java
@@ -10,5 +10,8 @@ public class C {
         System.out.println("Rajesh Roy");
         System.out.println("Deepak Kanathe");
         System.out.println("Shivam singh");
+        System.out.println("raja ram ki jay");
+        System.out.println("Shree radha krishna");
+
     }
 }
