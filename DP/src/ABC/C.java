e49e0aef1f470a603e58327a08ffadd7e06bc977
Pankaj <pankajuramaliya19@gmail.com>
2026-09-13 23:31:18 +0530

    add greetings radha and bappa

--- DP/src/ABC/C.java
+++ DP/src/ABC/C.java
@@ -15,10 +15,8 @@ public class C {
         System.out.println("raja ram ki jay");
         System.out.println("Shree radha krishna");
         System.out.println("Shivay");
-
-
-
-
+        System.out.println("radha rani");
+        System.out.println("Bappa moriya...");
 
     }
 }
